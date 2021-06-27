286c3a5600f6b6e13a7a06bf88aa04e16ab25ad8
Andrew Marsh <21085039+asmarsh@users.noreply.github.com>
2021-06-26 23:20:44 -0400

    Stubbing out interface and implementations

---
+++ FactoryDBProject/DataConnection/AccessConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDBProject.DataConnection
+{
+    internal class AccessConnection : IDataConnection
+    {
+        public bool Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
