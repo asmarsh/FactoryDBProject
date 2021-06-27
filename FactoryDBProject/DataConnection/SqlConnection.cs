e5c3f523f695a3932941c5d6e1ab6de28996b2ed
Andrew Marsh <21085039+asmarsh@users.noreply.github.com>
2021-06-26 23:30:46 -0400

    Moving current implementation to new class

--- FactoryDBProject/DataConnection/SqlConnection.cs
+++ FactoryDBProject/DataConnection/SqlConnection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FactoryDBProject.Data;
 
 namespace FactoryDBProject.DataConnection
 {
@@ -10,22 +11,117 @@ namespace FactoryDBProject.DataConnection
     {
         public bool Create()
         {
-            throw new NotImplementedException();
+            var vehicle = new VHT001_VEHICLE();
+            Console.WriteLine("Enter Vehicle Id:");
+            vehicle.VehicleId = Console.ReadLine();
+            Console.WriteLine("Enter Vehicle Color:");
+            vehicle.VehicleColor = Console.ReadLine();
+            Console.WriteLine("Enter Vehicle Type:");
+            var vehicleType = Console.ReadLine().ToUpper();
+            switch (vehicleType)
+            {
+                case "CAR":
+                    vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Car;
+                    break;
+
+                case "TRUCK":
+                    vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Truck;
+                    break;
+
+                case "BOAT":
+                    vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Boat;
+                    break;
+
+                default:
+                    vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Other;
+                    break;
+            }
+            vehicle.VehicleAddDateTime = DateTime.Now;
+
+            using (var ctx = new VehiclesContext())
+
+            {
+                ctx.VHT001_VEHICLE.Add(vehicle);
+                ctx.SaveChanges();
+                Console.WriteLine($"{nameof(vehicle.VehicleId)} {vehicle.VehicleId} added");
+            }
+
+            return true;
         }
 
         public bool Delete()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Enter vehicle id:");
+            var vehicleId = Console.ReadLine();
+
+            using (var ctx = new VehiclesContext())
+            {
+                var vehicleToDelete = ctx.VHT001_VEHICLE.FirstOrDefault(x => x.VehicleId == vehicleId);
+                if (vehicleToDelete == null) return false;
+                ctx.VHT001_VEHICLE.Remove(vehicleToDelete);
+                ctx.SaveChanges();
+            }
+
+            return true;
+
+            throw new ArgumentException($"{nameof(vehicleId)} {vehicleId} not found");
         }
 
         public bool Read()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Enter vehicle id:");
+            var vehicleId = Console.ReadLine();
+            VHT001_VEHICLE vehicle;
+            using (var ctx = new VehiclesContext())
+            {
+                vehicle = ctx.VHT001_VEHICLE.FirstOrDefault(x => x.VehicleId == vehicleId);
+            }
+            if (vehicle != null)
+            {
+                Console.WriteLine(vehicle.ToString());
+                return true;
+            }
+            throw new ArgumentException($"{nameof(vehicleId)} {vehicleId} not found");
         }
 
         public bool Update()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Enter vehicle id:");
+            var vehicleId = Console.ReadLine();
+            VHT001_VEHICLE vehicle;
+            using (var ctx = new VehiclesContext())
+            {
+                vehicle = ctx.VHT001_VEHICLE.FirstOrDefault(x => x.VehicleId == vehicleId);
+
+                if (vehicle != null)
+                {
+                    Console.WriteLine("Enter Vehicle Color:");
+                    vehicle.VehicleColor = Console.ReadLine();
+                    Console.WriteLine("Enter Vehicle Type:");
+                    var vehicleType = Console.ReadLine().ToUpper();
+                    switch (vehicleType)
+                    {
+                        case "CAR":
+                            vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Car;
+                            break;
+
+                        case "TRUCK":
+                            vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Truck;
+                            break;
+
+                        case "BOAT":
+                            vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Boat;
+                            break;
+
+                        default:
+                            vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Other;
+                            break;
+                    }
+                    ctx.SaveChanges();
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
--- FactoryDBProject/Program.cs
+++ FactoryDBProject/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FactoryDBProject.Data;
+using FactoryDBProject.DataConnection;
 
 namespace FactoryDBProject
 {
@@ -79,117 +80,30 @@ namespace FactoryDBProject
 
         private static bool AddVehicle()
         {
-            var vehicle = new VHT001_VEHICLE();
-            Console.WriteLine("Enter Vehicle Id:");
-            vehicle.VehicleId = Console.ReadLine();
-            Console.WriteLine("Enter Vehicle Color:");
-            vehicle.VehicleColor = Console.ReadLine();
-            Console.WriteLine("Enter Vehicle Type:");
-            var vehicleType = Console.ReadLine().ToUpper();
-            switch (vehicleType)
-            {
-                case "CAR":
-                    vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Car;
-                    break;
+            var reader = new SqlConnection();
 
-                case "TRUCK":
-                    vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Truck;
-                    break;
-
-                case "BOAT":
-                    vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Boat;
-                    break;
-
-                default:
-                    vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Other;
-                    break;
-            }
-            vehicle.VehicleAddDateTime = DateTime.Now;
-
-            using (var ctx = new VehiclesContext())
-
-            {
-                ctx.VHT001_VEHICLE.Add(vehicle);
-                ctx.SaveChanges();
-                Console.WriteLine($"{nameof(vehicle.VehicleId)} {vehicle.VehicleId} added");
-            }
-
-            return true;
+            return reader.Create();
         }
 
         private static bool UpdateVehicle()
         {
-            Console.WriteLine("Enter vehicle id:");
-            var vehicleId = Console.ReadLine();
-            VHT001_VEHICLE vehicle;
-            using (var ctx = new VehiclesContext())
-            {
-                vehicle = ctx.VHT001_VEHICLE.FirstOrDefault(x => x.VehicleId == vehicleId);
+            var reader = new SqlConnection();
 
-                if (vehicle != null)
-                {
-                    Console.WriteLine("Enter Vehicle Color:");
-                    vehicle.VehicleColor = Console.ReadLine();
-                    Console.WriteLine("Enter Vehicle Type:");
-                    var vehicleType = Console.ReadLine().ToUpper();
-                    switch (vehicleType)
-                    {
-                        case "CAR":
-                            vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Car;
-                            break;
-
-                        case "TRUCK":
-                            vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Truck;
-                            break;
-
-                        case "BOAT":
-                            vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Boat;
-                            break;
-
-                        default:
-                            vehicle.VehicleTypeCode = (int?)Constants.VehicleType.Other;
-                            break;
-                    }
-                    ctx.SaveChanges();
-                    return true;
-                }
-            }
-            return false;
+            return reader.Update();
         }
 
         private static bool RetrieveVehicle()
         {
-            Console.WriteLine("Enter vehicle id:");
-            var vehicleId = Console.ReadLine();
-            VHT001_VEHICLE vehicle;
-            using (var ctx = new VehiclesContext())
-            {
-                vehicle = ctx.VHT001_VEHICLE.FirstOrDefault(x => x.VehicleId == vehicleId);
-            }
-            if (vehicle != null)
-            {
-                Console.WriteLine(vehicle.ToString());
-                return true;
-            }
-            throw new ArgumentException($"{nameof(vehicleId)} {vehicleId} not found");
+            var reader = new SqlConnection();
+
+            return reader.Read();
         }
 
         private static bool DeleteVehicle()
         {
-            Console.WriteLine("Enter vehicle id:");
-            var vehicleId = Console.ReadLine();
+            var reader = new SqlConnection();
 
-            using (var ctx = new VehiclesContext())
-            {
-                var vehicleToDelete = ctx.VHT001_VEHICLE.FirstOrDefault(x => x.VehicleId == vehicleId);
-                if (vehicleToDelete == null) return false;
-                ctx.VHT001_VEHICLE.Remove(vehicleToDelete);
-                ctx.SaveChanges();
-            }
-
-            return true;
-
-            throw new ArgumentException($"{nameof(vehicleId)} {vehicleId} not found");
+            return reader.Delete();
         }
     }
 }
